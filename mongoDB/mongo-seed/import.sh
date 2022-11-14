b00a0095a96668827a249c02137041373c443c88
miwox <miwand.b@live.de>
2022-11-14 18:49:57 +0100

    Welt

--- mongoDB/mongo-seed/import.sh
+++ mongoDB/mongo-seed/import.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-mongoimport --host mongodb --db mongodb --collection census --type json --file /mongo-seed/census.json
+FILES="/mongo-seed/*.json"
+prefix="/mongo-seed/"
+suffix=".json"
+echo "Trying to import to MongoDB..."
+for f in $FILES
+do
+    filename=${f#"$prefix"}
+    echo "Importing ${filename}."
+    collectionname=${filename%"$suffix"}
+    {
+        mongoimport --host mongodb --db mongodb --collection $collectionname --type json --file /mongo-seed/$filename --jsonArray
+    } 1>/dev/null 2>&1
+    echo "${filename} Import successfull."
+    echo ""
+done
+echo "Import done."
